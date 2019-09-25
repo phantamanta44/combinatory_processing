ec54de1a5ea91f19867d795daea27a402d6a89c8
phantamanta44 <evandalong@gmail.com>
2019-09-25 18:50:55 -0500

    Properly handle topic datasources with intermediate processing pipeline segments

--- src/combinatory_processing/main.py
+++ src/combinatory_processing/main.py
@@ -5,28 +5,31 @@ from .source import DataSourceTopic
 class Subscription:
     def __init__(self, msg_type):
         self.msg_type = msg_type
-        self.pipelines = []
+        self.sources = []
 
     def accept(self, msg):
-        for pipeline in self.pipelines:
-            pipeline.source.enqueue(msg)
+        for source in self.sources:
+            source.enqueue(msg)
 
 
 def spin(name, pipelines, anon=False, freq=30):
     rospy.init_node(name, anonymous=anon)
 
     subs = {}
+    seen_sources = set()
     for pipeline in pipelines:
-        source = pipeline.source
-        if isinstance(source, DataSourceTopic):
-            if source.topic_name not in subs:
-                sub = Subscription(source.msg_type)
-                rospy.Subscriber(source.topic_name, source.msg_type, callback=sub.accept)
-                subs[source.topic_name] = sub
-            elif subs[source.topic_name].msg_type != source.msg_type:
-                raise RuntimeError('Topic type mismatch for %s: %s != %s' % (
-                    source.topic_name, subs[source.topic_name].msg_type, source.msg_type))
-            subs[source.topic_name].pipelines.append(pipeline)
+        for source in pipeline.source_set:
+            if isinstance(source, DataSourceTopic) and source not in seen_sources:
+                print(str(type(source)) + ' is a DataSourceTopic: ' + str(isinstance(source, DataSourceTopic)))
+                seen_sources.add(source)
+                if source.topic_name not in subs:
+                    sub = Subscription(source.msg_type)
+                    rospy.Subscriber(source.topic_name, source.msg_type, callback=sub.accept)
+                    subs[source.topic_name] = sub
+                elif subs[source.topic_name].msg_type != source.msg_type:
+                    raise RuntimeError('Topic type mismatch for %s: %s != %s' % (
+                        source.topic_name, subs[source.topic_name].msg_type, source.msg_type))
+                subs[source.topic_name].sources.append(source)
 
     poll_time = 0
     sleeper = rospy.Rate(freq)
--- src/combinatory_processing/pipeline.py
+++ src/combinatory_processing/pipeline.py
@@ -3,11 +3,12 @@ from .source import *
 
 
 class PipelineSegment:
-    def __init__(self, source):
+    def __init__(self, source, source_hierarchy=[]):
         self.source = source
+        self.source_hierarchy = source_hierarchy + [source]
 
     def map(self, mapping_func):
-        return PipelineSegment(DataSourceMapping(self, mapping_func))
+        return PipelineSegment(DataSourceMapping(self, mapping_func), self.source_hierarchy)
 
     def join(self, others, joining_func, buffer_size=4):
         pipelines = [self]
@@ -16,7 +17,7 @@ class PipelineSegment:
                 pipelines.append(other)
         except TypeError:
             pipelines.append(others)
-        return PipelineSegment(DataSourceConjoining(pipelines, joining_func, buffer_size))
+        return PipelineSegment(DataSourceConjoining(pipelines, joining_func, buffer_size), self.source_hierarchy)
 
     def sink(self, topic, msg_type):
         return Pipeline(self, topic, msg_type)
@@ -25,6 +26,7 @@ class PipelineSegment:
 class Pipeline:
     def __init__(self, pipeline, sink_topic, sink_type):
         self.source = pipeline.source
+        self.source_set = set(pipeline.source_hierarchy)
         self.dest = rospy.Publisher(sink_topic, sink_type, queue_size=8)
 
     def consume(self, msg):
